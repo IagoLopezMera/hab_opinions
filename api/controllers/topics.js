3551a7f5f388702ff6d1dfcea5181bdc27c5d9af
Alexander <50184502+alexander970@users.noreply.github.com>
2022-05-03 21:35:54 +0200

    overall improvements

--- api/controllers/topics.js
+++ api/controllers/topics.js
@@ -6,10 +6,12 @@ const {
   updateTopic,
 } = require('../db/topics');
 
+const { topicValidator } = require('../validators/topics');
+
 //Get all topics controller
 const getAllTopicsController = async (req, res) => {
   const topics = await getAllTopics();
-  res.send(topics);
+  res.send({ status: 'ok', topics });
 };
 
 //Get topics by id controller
@@ -17,24 +19,39 @@ const getTopicByIdController = async (req, res) => {
   const id = Number(req.params.id);
 
   const topic = await getTopicById(id);
+
   if (!topic)
-    return res
-      .status(404)
-      .send({ message: 'The topic with the given ID was not found' });
+    return res.status(404).send({
+      status: 'error',
+      message: 'The topic with the given ID was not found',
+    });
 
   res.send(topic);
 };
 
 // Create a topic
 const createTopicController = async (req, res) => {
+  const validation = topicValidator.validate(req.body);
+
+  if (validation.error)
+    return res
+      .status(400)
+      .send({ status: 'error', message: validation.error.message });
+
   const { description } = req.body;
 
   const topic = await getTopicByDescription(description);
+
   if (topic)
     return res.send({ message: 'The topic already exists', topic: topic });
 
   const id = await createTopic(description);
-  res.send({ message: `The topic with the ID: ${id} has been created`,topic:id });
+
+  res.send({
+    status: 'ok',
+    message: `The topic with the ID: ${id} has been created`,
+    topic: id,
+  });
 };
 
 //Update topic
@@ -43,20 +60,29 @@ const updateTopicController = async (req, res) => {
   const { description: newDescription } = req.body;
 
   const topicById = await getTopicById(id);
+
   if (!topicById)
-    return res
-      .status(404)
-      .send({ message: 'The topic with the given ID was not found' });
+    return res.status(404).send({
+      status: 'error',
+      message: 'The topic with the given ID was not found',
+    });
 
   const topicByDescription = await getTopicByDescription(newDescription);
+
   if (topicByDescription)
     return res.send({
+      status: 'error',
       message: 'A topic with the new description already exists',
       topic: topicByDescription,
     });
 
   await updateTopic(id, newDescription);
-  res.send({ message: `The topic with the ID: ${id} has been updated`, topic: id});
+
+  res.send({
+    status: 'ok',
+    message: `The topic with the ID: ${id} has been updated`,
+    topic: id,
+  });
 };
 
 module.exports = {
--- api/index.js
+++ api/index.js
@@ -3,37 +3,37 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
-const { 
-    getAllTopicsController,
-    getTopicByIdController,
-    createTopicController,
-    updateTopicController
+const {
+  getAllTopicsController,
+  getTopicByIdController,
+  createTopicController,
+  updateTopicController,
 } = require('./controllers/topics');
 
-const { 
-    getAllOpinionRatingsController,
-    getOpinionRatingByIdController,
-    createOpinionRatingController,
-    updateOpinionRatingController
+const {
+  getAllOpinionRatingsController,
+  getOpinionRatingByIdController,
+  createOpinionRatingController,
+  updateOpinionRatingController,
 } = require('./controllers/ratings');
 
 const {
-    newUserController,
-    getUsersController,
-    getSingleUserController,
-    loginController,
-    modifyUserController,
+  newUserController,
+  getUsersController,
+  getSingleUserController,
+  loginController,
+  modifyUserController,
 } = require('./controllers/users');
 
 const {
-    getOpinionsController,
-    newOpinionController,
-    getSingleOpinionController,
-    deleteOpinionController,
-    modifyOpinionController,
-} = require('./controllers/opinions.js')
+  getOpinionsController,
+  newOpinionController,
+  getSingleOpinionController,
+  deleteOpinionController,
+  modifyOpinionController,
+} = require('./controllers/opinions.js');
 
-const {authUser} = require('./middlewares/auth');
+const { authUser } = require('./middlewares/auth');
 
 const app = express();
 
@@ -44,45 +44,45 @@ app.use(morgan('dev'));
 app.get('/api/topics', getAllTopicsController);
 app.get('/api/topics/:id', getTopicByIdController);
 app.post('/api/topics', createTopicController);
-app.put('/api/topics/:id', updateTopicController);
+app.put('/api/topics/:id', authUser, updateTopicController);
 
 //Ratings routes
-app.get('/api/opinions/:idOpinion/ratings',getAllOpinionRatingsController);
-app.get('/api/opinions/:idOpinion/ratings/:idUser',getOpinionRatingByIdController);
-app.post('/api/opinions/:idOpinion/ratings',createOpinionRatingController);
-app.patch('/api/opinions/:idOpinion/ratings',updateOpinionRatingController);
+app.get('/api/opinions/:idOpinion/ratings', getAllOpinionRatingsController);
+app.get('/api/opinions/:idOpinion/ratings/:idUser', getOpinionRatingByIdController);
+app.post('/api/opinions/:idOpinion/ratings', authUser, createOpinionRatingController);
+app.patch('/api/opinions/:idOpinion/ratings', authUser, updateOpinionRatingController);
 
 // ENDPOINTS DE USERS
 app.post('/api/users', newUserController);
 app.get('/api/users', getUsersController);
 app.get('/api/users/:id', getSingleUserController);
 app.post('/api/login', loginController);
-app.patch('/api/users/:id',authUser, modifyUserController);
+app.patch('/api/users/:id', authUser, modifyUserController);
 
 // ENDPOINTS DE OPINIONES
 app.post('/api/opinions', authUser, newOpinionController);
 app.get('/api/opinions', getOpinionsController);
 app.get('/api/opinions/:id', getSingleOpinionController);
-app.delete('/api/opinions/:id',authUser, deleteOpinionController);
-app.patch('/api/opinions/:id',authUser, modifyOpinionController);
+app.delete('/api/opinions/:id', authUser, deleteOpinionController);
+app.patch('/api/opinions/:id', authUser, modifyOpinionController);
 
 // Middleware de 404
 app.use((req, res) => {
-    res.status(404).send({
-        status: 'error',
-        message: 'Not found',
-    });
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found',
+  });
 });
 
 // Middleware de gestión de errores
 app.use((error, req, res, next) => {
-    console.error(error);
+  console.error(error);
 
-    res.status(error.httpStatus || 500).send({
-        status: 'error',
-        message: error.message,      
-    });
+  res.status(error.httpStatus || 500).send({
+    status: 'error',
+    message: error.message,
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port,() => console.log(`listening on port:${port}`));
+app.listen(port, () => console.log(`listening on port:${port}`));
