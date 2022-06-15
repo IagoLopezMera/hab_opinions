a33f2ce2be46b234cd15a18c83432640f0a0b0ee
Iago <iago.lopezmera@protonmail.com>
2022-06-15 21:22:50 +0200

    topics asks

--- api/controllers/opinions.js
+++ api/controllers/opinions.js
@@ -1,116 +1,113 @@
-const { createOpinion, getAllOpinions, getOpinionById, deleteOpinionById, modifyOpinionById } = require('../db/opinions');
-const {generateError} = require('../helpers');
-
-
+const {
+  createOpinion,
+  getAllOpinions,
+  getOpinionById,
+  deleteOpinionById,
+  modifyOpinionById,
+} = require('../db/opinions');
+const { generateError } = require('../helpers');
 
 const getOpinionsController = async (req, res, next) => {
-
-    try {
-        const opinions = await getAllOpinions();
-        res.send({
-            status: 'ok',
-            data: opinions,
-        })
-    } catch(error) {
-        next(error);
-    }
+  try {
+    const opinions = await getAllOpinions();
+    res.send({
+      status: 'ok',
+      data: opinions,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 const newOpinionController = async (req, res, next) => {
-    
-    
-    try {
-        const { text, topic } =req.body;
-        
-        if (!text) {
-            throw generateError(
-                'El texto no puede estar vacío', 400
-            );
-        }
-    
-        const id = await createOpinion(topic, req.idUser, text);
-        res.send({
-            status: 'ok',
-            data: `Opinión con id: ${id} creada correctamente`,
-        })
-    } catch(error) {
-        next(error);
+  try {
+    const { text, topic } = req.body;
+
+    if (!text) {
+      throw generateError('El texto no puede estar vacío', 400);
     }
 
+    const id = await createOpinion(topic, req.idUser, text);
+    res.send({
+      status: 'ok',
+      data: `Opinión con id: ${id} creada correctamente`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getSingleOpinionController = async (req, res, next) => {
-    try {
+  try {
+    const { id } = req.params;
 
-        const { id } = req.params
-
-        const opinion = await getOpinionById(id)
-        res.send({
-            status: 'ok',
-            data: opinion,
-        })
-    } catch(error) {
-        next(error);
-    }
+    const opinion = await getOpinionById(id);
+    console.log(opinion);
+    res.send({
+      status: 'ok',
+      data: opinion,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const deleteOpinionController = async (req, res, next) => {
-    try {
-        // req.OpinionId
-        const { id } = req.params;
-        
-        // Conseguir la información de la opinión que se quiere eliminar
-        const opinion = await getOpinionById(id);
+  try {
+    // req.OpinionId
+    const { id } = req.params;
 
-        // Comprobar que el usario del token es el mismo que creó la opinión
-        if (req.idUser !== opinion.idUser) {
-            throw generateError(
-                'No es posible eliminar una opinión de otro usuario',401
-            )
-        }
+    // Conseguir la información de la opinión que se quiere eliminar
+    const opinion = await getOpinionById(id);
 
-        // Eliminar la opinión
-        await deleteOpinionById(id);
-
-        res.send({
-            status: 'ok',
-            data: `La opinión con id: ${id} ha sido eliminada.`,
-        })
-    } catch(error) {
-        next(error);
+    // Comprobar que el usario del token es el mismo que creó la opinión
+    if (req.idUser !== opinion.idUser) {
+      throw generateError(
+        'No es posible eliminar una opinión de otro usuario',
+        401
+      );
     }
+
+    // Eliminar la opinión
+    await deleteOpinionById(id);
+
+    res.send({
+      status: 'ok',
+      data: `La opinión con id: ${id} ha sido eliminada.`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const modifyOpinionController = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const { text } = req.body;
-        const opinion = await getOpinionById(id);
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    const opinion = await getOpinionById(id);
 
-        if (req.idUser !== opinion.idUser) {
-            throw generateError(
-                'No es posible modificar una opinión de otro usuario'
-            )
-        }
-
-        // Modificar la opinión
-        await modifyOpinionById(id, text);
-
-        res.send({
-            status: 'ok',
-            data: `El texto con id: ${id} se ha modificado`,
-        })
-    } catch(error) {
-        next(error);
+    if (req.idUser !== opinion.idUser) {
+      throw generateError(
+        'No es posible modificar una opinión de otro usuario'
+      );
     }
+
+    // Modificar la opinión
+    await modifyOpinionById(id, text);
+
+    res.send({
+      status: 'ok',
+      data: `El texto con id: ${id} se ha modificado`,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-
 module.exports = {
-    getOpinionsController,
-    newOpinionController,
-    getSingleOpinionController,
-    deleteOpinionController,
-    modifyOpinionController,
+  getOpinionsController,
+  newOpinionController,
+  getSingleOpinionController,
+  deleteOpinionController,
+  modifyOpinionController,
 };
--- api/db/opinions.js
+++ api/db/opinions.js
@@ -13,114 +13,115 @@ const getOpinionsByUserId = async (id) => {
       SELECT Opinion.*, User.username FROM Opinion LEFT JOIN User on Opinion.idUser = User.idUser WHERE Opinion.idUser = ?`,
       [id]
     );
-    
+
     return result;
   } finally {
     if (connection) connection.release();
   }
-}
+};
 
 const getAllOpinions = async () => {
-    let connection;
+  let connection;
 
-    try {
-        connection = await getConnection();
+  try {
+    connection = await getConnection();
 
-        const [result] = await connection.query(`
-        SELECT Opinion.idOpinion, Opinion.text, Opinion.idTopic, Opinion.idUser, Opinion.createdAt, User.username FROM Opinion LEFT JOIN User on Opinion.idUser = User.idUser ORDER BY Opinion.createdAt DESC;
+    const [result] = await connection.query(`
+        SELECT Opinion.idOpinion, Opinion.text, Opinion.idTopic, Topic.description topicDescription, Opinion.idUser, Opinion.createdAt, User.username FROM Opinion INNER JOIN Topic on Opinion.idTopic = Topic.idTopic LEFT JOIN User on Opinion.idUser = User.idUser ORDER BY Opinion.createdAt DESC;
         `);
-        
-        return result;
-    } finally {
-        if(connection) connection.release();
-    }
-}
 
+    return result;
+  } finally {
+    if (connection) connection.release();
+  }
+};
 
 const getOpinionById = async (id) => {
-    let connection;
+  let connection;
 
-    try {
-        connection = await getConnection();
+  try {
+    connection = await getConnection();
 
-        const [result] = await connection.query(`
-        SELECT Opinion.idOpinion, Opinion.text, Opinion.idTopic, Opinion.idUser, Opinion.createdAt, User.username FROM Opinion LEFT JOIN User on Opinion.idUser = User.idUser WHERE idOpinion = ?
-        `, [id]);
+    const [result] = await connection.query(
+      `
+        SELECT Opinion.idOpinion, Opinion.text, Opinion.idTopic, Topic.description topicDescription, Opinion.idUser, Opinion.createdAt, User.username FROM Opinion INNER JOIN Topic on Opinion.idTopic = Topic.idTopic LEFT JOIN User on Opinion.idUser = User.idUser WHERE idOpinion = ?
+        `,
+      [id]
+    );
 
-        if(!result.length) {
-          throw generateError('La opinión no existe', 404);
-        }
-
-        return result[0];
-    } finally {
-        if(connection) connection.release();
+    if (!result.length) {
+      throw generateError('La opinión no existe', 404);
     }
-}
+
+    return result[0];
+  } finally {
+    if (connection) connection.release();
+  }
+};
 
 const createOpinion = async (idTopic, idUser, text) => {
-    let connection;
+  let connection;
 
-    try {
-        connection = await getConnection();
+  try {
+    connection = await getConnection();
 
-        const [result] = await connection.query(`
+    const [result] = await connection.query(
+      `
         INSERT INTO Opinion (idTopic, idUser, text)
         VALUES(?, ?, ?);
-        `, 
-        [idTopic, idUser, text]
-        );
+        `,
+      [idTopic, idUser, text]
+    );
 
-        return result.insertId;
-
-    } finally {
-        if (connection) connection.release()
-    }
-
+    return result.insertId;
+  } finally {
+    if (connection) connection.release();
+  }
 };
 
 const deleteOpinionById = async (id) => {
-    let connection;
-  
-    try {
-      connection = await getConnection();
-  
-      await connection.query(
-        `
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    await connection.query(
+      `
         DELETE FROM Opinion WHERE idOpinion = ?
       `,
-        [id]
-      );
-  
-      return;
-    } finally {
-      if (connection) connection.release();
-    }
+      [id]
+    );
+
+    return;
+  } finally {
+    if (connection) connection.release();
+  }
 };
 
 const modifyOpinionById = async (idOpinion, text) => {
-    let connection;
-  
-    try {
-      connection = await getConnection();
-  
-      await connection.query(
-        `
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    await connection.query(
+      `
         UPDATE Opinion SET text = ? WHERE idOpinion = ?
       `,
-        [text, idOpinion]
-      );
-  
-      return;
-    } finally {
-      if (connection) connection.release();
-    }
+      [text, idOpinion]
+    );
+
+    return;
+  } finally {
+    if (connection) connection.release();
+  }
 };
 
 module.exports = {
-    createOpinion,
-    getAllOpinions,
-    getOpinionById,
-    deleteOpinionById,
-    modifyOpinionById,
-    getOpinionsByUserId
+  createOpinion,
+  getAllOpinions,
+  getOpinionById,
+  deleteOpinionById,
+  modifyOpinionById,
+  getOpinionsByUserId,
 };
